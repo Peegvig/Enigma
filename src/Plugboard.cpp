9be6dcc29972326d8385a6d5792a620b9172ecfc
Peegvig <arumallavihal@gmail.com>
2025-10-17 17:34:01 -0500

    changed file structure

--- src/Plugboard.cpp
+++ src/Plugboard.cpp
@@ -1,32 +1,32 @@
-#include "/home/csce120/Enigma/header/Plugboard.h"
-#include <string>
-#include <sstream>
-#include <iostream>
-Plugboard::Plugboard(){
-        for(char c = 'A'; c< 'Z';c++ ){
-                Plugboard::connections.at(c) = c;
-        }
-}
+// #include "Plugboard.h"
+// #include <string>
+// #include <sstream>
+// #include <iostream>
+// Plugboard::Plugboard(){
+//         for(char c = 'A'; c< 'Z';c++ ){
+//                 Plugboard::connections.at(c) = c;
+//         }
+// }
 
-Plugboard::Plugboard(std::string initString){
-        std::stringstream s;
-        s<<initString;
-        std::string pair;
-        while(s>>pair){
-                Plugboard::connections.at(pair.at(0)) = pair.at(1);
-                Plugboard::connections.at(pair.at(1)) = pair.at(0);
-        }
+// Plugboard::Plugboard(std::string initString){
+//         std::stringstream s;
+//         s<<initString;
+//         std::string pair;
+//         while(s>>pair){
+//                 Plugboard::connections.at(pair.at(0)) = pair.at(1);
+//                 Plugboard::connections.at(pair.at(1)) = pair.at(0);
+//         }
 
-}
+// }
 
-char Plugboard::process(char c){
-        return Plugboard::connections.at(c);
-}
+// char Plugboard::process(char c){
+//         return Plugboard::connections.at(c);
+// }
 
-std::string Plugboard::toString(){
-        std::string s;
-        for(auto it = Plugboard::connections.begin(); it != Plugboard::connections.end();it++){
-                s += std::to_string(it->first) + " is mapped to " + std::to_string(it->second);
-        }
-}
+// std::string Plugboard::toString(){
+//         std::string s;
+//         for(auto it = Plugboard::connections.begin(); it != Plugboard::connections.end();it++){
+//                 s += std::to_string(it->first) + " is mapped to " + std::to_string(it->second);
+//         }
+// }
 
